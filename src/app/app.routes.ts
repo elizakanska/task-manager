9d863b699b38dba78d1a1aaed0d6ae87bdbd3ead
Elīza <elizakanska@gmail.com>
2025-03-03 16:26:33 +0200

    TM-4 (TM-5,6,7) Added component for tasks (list, add, delete, edit, search) Will figure out something cute to do with welcome component later

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import {WelcomeComponent} from './pages/welcome/welcome.component';
+import {TasksComponent} from './pages/tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
-];
+  { path: 'welcome', component: WelcomeComponent},
+  { path: 'tasks', component: TasksComponent},
+]
