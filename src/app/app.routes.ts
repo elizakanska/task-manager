e5533eaf773d476b645588732bb07af60b13ec15
Elīza Kanska 221RDB095 <78021756+elizakanska@users.noreply.github.com>
2025-03-03 16:29:51 +0200

    Fixed leftovers
    Had some leftovers from a failed test, forgot them in the initial commit, fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {TasksComponent} from './pages/tasks/tasks.component';
-import {AddComponent} from './pages/add/add.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', component: WelcomeComponent},
   { path: 'tasks', component: TasksComponent},
-  { path: 'add', component: AddComponent},
 ]
