0c239100609da1950c169fec567501217ffad373
Elīza <elizakanska@gmail.com>
2025-03-18 10:24:01 +0200

    Added FE user endpoints (also a quick user list)

--- FE/src/app/app.routes.ts
+++ FE/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {WelcomeComponent} from './pages/welcome/welcome.component';
 import {TasksComponent} from './pages/tasks/tasks.component';
+import {UsersComponent} from './pages/users/users.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', component: WelcomeComponent},
   { path: 'tasks', component: TasksComponent},
+  { path: 'users', component: UsersComponent},
 ]
