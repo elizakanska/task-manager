b20663c7be8509e23f49e956a42b85338b60b9bf
Elīza <elizakanska@gmail.com>
2025-03-18 11:18:00 +0200

    Switched the DTO and Entity names and positions around to be more accurate to their role in the application

--- src/main/java/com/kanskaeliza/taskmanager/mapper/TaskMapper.java
+++ src/main/java/com/kanskaeliza/taskmanager/mapper/TaskMapper.java
@@ -1,15 +1,15 @@
 package com.kanskaeliza.taskmanager.mapper;
 
-import com.kanskaeliza.taskmanager.entity.Task;
 import com.kanskaeliza.taskmanager.entity.dto.TaskDTO;
+import com.kanskaeliza.taskmanager.entity.Task;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
     @Mapping(target = "id", source = "task.id")
-    TaskDTO toDto(Task task);
+    Task fromDto(TaskDTO taskdto);
 
     @Mapping(target = "id", source = "id")
-    Task toTask(TaskDTO dto);
+    TaskDTO fromTask(Task task);
 }
--- src/main/java/com/kanskaeliza/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/kanskaeliza/taskmanager/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package com.kanskaeliza.taskmanager.repository;
 
-import com.kanskaeliza.taskmanager.entity.dto.TaskDTO;
+import com.kanskaeliza.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<TaskDTO, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
